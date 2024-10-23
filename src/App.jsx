517b303bedc9bf0d2888ffb18d3d7cee7e4c6faf
BulatGarifullin <130639959+BulatGarifullin@users.noreply.github.com>
2024-10-23 18:32:09 +0300

    push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+import ReactLogo from './assets/react.svg?react';
+
+export const App = () => {
+	return (
+		<div>
+			<div className={styles.App}>123</div>
+			<ReactLogo />
+		</div>
+	);
+};
